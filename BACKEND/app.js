4586b62d841596c39748809d509fe6cb864017c8
rituraj <rituraj821310@gmail.com>
2026-07-13 08:51:20 +0530

    Allow Vercel frontend

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-vercel-app.vercel.app",
+      "https://shortify-iota-two.vercel.app",
     ],
     credentials: true,
   })
